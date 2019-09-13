99623c670add9e8fad1373e32448a48308e32bf4
Ben Nunes <ifrn.edu@hotmail.com>
2019-09-12 23:31:58 -0300

    Update pergunta4.js

--- pergunta4.js
+++ pergunta4.js
@@ -1,1 +1,14 @@
+var x = [ 1, 6, 3 ]; 
+organiza(x);
 
+function organiza(params) {
+  for (i=0; i<params.length; i++) {
+    if(params[i]>params[i+1]){
+		var aux = params[i+1]
+		params[i+1] = params[i]
+		params[i] = aux  
+	}
+	
+  }
+console.log(params)
+}
